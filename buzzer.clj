e7ad9419ddca91b189fdd847667d0e7a42f84433
Larry Diehl <larrytheliquid@kubuntutheliquid.(none)>
2009-02-26 04:26:13 -0500

    Added buzzword? test for when the buzzword is contained in the candidate string.
    Introduced that Clojure strings are Java string objects, then called the Java String "contains" method on our candidate object with buzzword as the parameter.

--- buzzer.clj
+++ buzzer.clj
@@ -1,7 +1,7 @@
 (ns buzzer (:use fact))
 
 (defn buzzword? [candidate buzzword]
-  (= candidate buzzword))
+  (. candidate (contains buzzword)))
 
 (def buzzwords ["ajax" "dsl" "rest"])
 (def non-buzzwords ["apple" "cat" "house"])
@@ -9,6 +9,11 @@
 (fact "buzzword?: true when string equals given buzzword" 
   [buzzword buzzwords]
   (buzzword? buzzword buzzword))
+
+(fact "buzzword?: true when string contains given buzzword" 
+  [candidate ["start ajax end" "start dsl end" "start rest end"]
+   buzzword buzzwords]
+  (buzzword? candidate buzzword))
 
 (fact "buzzword?: false when string does not equal given buzzword" 
   [candidate non-buzzwords
