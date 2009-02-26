3bf5513c56f80d02efc448ad30117ef0d9c4f5bf
Larry Diehl <larrytheliquid@kubuntutheliquid.(none)>
2009-02-26 03:04:37 -0500

    Started using the "fact" unit testing library

--- buzzer.clj
+++ buzzer.clj
@@ -1,8 +1,14 @@
-(ns buzzer)
+(ns buzzer (:use fact))
 
 (defn buzzword? [candidate buzzword]
   true)
 
-;;; true when string equals given buzzword
-(prn (buzzword? "ajax" "ajax"))
-;;; true
+(fact "buzzword?: true when string equals given buzzword" []
+  (buzzword? "ajax" "ajax"))
+
+(.println *test-out* "buzzer:")
+(print-color-results (verify-facts 'buzzer))
+(.println *test-out*)
+;; buzzer:
+;; - buzzword?: true when string equals given buzzword
+;; 1 facts, 0 pending, 0 failed, 0 exceptions
