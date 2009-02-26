de61e7190fa2e49db123b57a69a0f23c67cd2847
Larry Diehl <larrytheliquid@kubuntutheliquid.(none)>
2009-02-26 03:32:48 -0500

    Create an infinite sequence of "ajax" strings, and take 3 of them for our test.

--- buzzer.clj
+++ buzzer.clj
@@ -9,13 +9,9 @@
 
 (fact "buzzword?: false when string does not equal given buzzword" 
   [candidate ["apple" "cat" "house"]
-   buzzword ["ajax" "ajax" "ajax"]]
+   buzzword (take 3 (repeat "ajax"))]
   (false? (buzzword? candidate buzzword)))
 
 (.println *test-out* "buzzer:")
 (print-color-results (verify-facts 'buzzer))
 (.println *test-out*)
-;; buzzer:
-;; - buzzword?: false when string does not equal given buzzword
-;; - buzzword?: true when string equals given buzzword
-;; 2 facts, 0 pending, 0 failed, 0 exceptions
