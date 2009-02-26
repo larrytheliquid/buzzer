eddaa0dd128b8036824fc842cf1ab41e5ba41183
Larry Diehl <larrytheliquid@kubuntutheliquid.(none)>
2009-02-26 02:47:31 -0500

    Added call to buzzword? function

---
+++ buzzer.clj
@@ -0,0 +1,6 @@
+;;; true when string equals given buzzword
+(buzzword? "ajax" "ajax")
+
+;; Exception in thread "main" java.lang.Exception: 
+;; Unable to resolve symbol: buzzword? 
+;; in this context (buzzer.clj:2)
