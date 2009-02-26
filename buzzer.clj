d2fdf6e2bb1aefdf6871e4957b8346b3c4b526e9
Larry Diehl <larrytheliquid@kubuntutheliquid.(none)>
2009-02-26 03:10:51 -0500

    Test multiple buzzwords in buzzword? function, and demonstrate fact's test templating ability

--- buzzer.clj
+++ buzzer.clj
@@ -3,12 +3,10 @@
 (defn buzzword? [candidate buzzword]
   true)
 
-(fact "buzzword?: true when string equals given buzzword" []
-  (buzzword? "ajax" "ajax"))
+(fact "buzzword?: true when string equals given buzzword" 
+  [buzzword (vector "ajax" "dsl" "rest")]
+  (buzzword? buzzword buzzword))
 
 (.println *test-out* "buzzer:")
 (print-color-results (verify-facts 'buzzer))
 (.println *test-out*)
-;; buzzer:
-;; - buzzword?: true when string equals given buzzword
-;; 1 facts, 0 pending, 0 failed, 0 exceptions
