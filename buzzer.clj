a3830f3324dcf6b5f5f7aa973d816bbcb8efc771
Larry Diehl <larrytheliquid@kubuntutheliquid.(none)>
2009-02-26 02:49:37 -0500

    Made buzzword? not raise an exception

--- buzzer.clj
+++ buzzer.clj
@@ -1,6 +1,5 @@
+(defn buzzword? [candidate buzzword]
+  false)
+
 ;;; true when string equals given buzzword
 (buzzword? "ajax" "ajax")
-
-;; Exception in thread "main" java.lang.Exception: 
-;; Unable to resolve symbol: buzzword? 
-;; in this context (buzzer.clj:2)
