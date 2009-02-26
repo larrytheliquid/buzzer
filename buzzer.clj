a179d772e49b2f37dab0457663a6d1d3c4c2c517
Larry Diehl <larrytheliquid@kubuntutheliquid.(none)>
2009-02-26 03:20:05 -0500

    Added the false testcase for buzzword?, creating a test failure. Also demonstrates fact's ability to have multiple local variable arguments in tests.

--- buzzer.clj
+++ buzzer.clj
@@ -7,6 +7,16 @@
   [buzzword ["ajax" "dsl" "rest"]]
   (buzzword? buzzword buzzword))
 
+(fact "buzzword?: false when string does not equal given buzzword" 
+  [candidate ["apple" "cat" "house"]
+   buzzword ["ajax" "ajax" "ajax"]]
+  (false? (buzzword? candidate buzzword)))
+
 (.println *test-out* "buzzer:")
 (print-color-results (verify-facts 'buzzer))
 (.println *test-out*)
+;; buzzer:
+;; - buzzword?: false when string does not equal given buzzword
+;;   FAILURE WHEN: candidate = "apple", buzzword = "ajax"
+;; - buzzword?: true when string equals given buzzword
+;; 2 facts, 0 pending, 1 failed, 0 exceptions
